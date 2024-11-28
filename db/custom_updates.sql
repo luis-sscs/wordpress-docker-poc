4148d658d90029c5b7a1bb251d34dc3c6f4084b0
Luis Silva <lsilva@residentinterface.com>
2024-11-27 21:44:50 -0500

    Initial setup to host wordpress in docker containers

---
+++ db/custom_updates.sql
@@ -0,0 +1,9 @@
+
+-- Need to hard code the internatl site url since the appraoch using ENV variables did not work...
+UPDATE wp_options 
+    SET option_value = 'http://localhost:4001'
+    WHERE option_name = 'home';
+
+UPDATE wp_options
+    SET option_value =  'http://localhost:4001'
+    WHERE option_name = 'siteurl';
