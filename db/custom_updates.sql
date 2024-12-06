564efc014a5f19e96bbabd67503925d50b1bc9ff
Luis Silva <lsilva@residentinterface.com>
2024-12-05 22:36:08 -0500

    Scrubbing default user to keep having default

--- db/custom_updates.sql
+++ db/custom_updates.sql
@@ -6,3 +6,13 @@ UPDATE wp_options
 UPDATE wp_options
     SET option_value =  'http://localhost:2000'
     WHERE option_name = 'siteurl';
+
+
+-- Scrubbing default user to keep havinf=g default password:  AdminLogin123
+UPDATE `wp_users` 
+    SET `user_pass` = '$P$BA0El8cveWN9i9fi5k2VJFGSOLiyTF/'
+WHERE (`user_login` = 'admin-user');
+
+-- Changing sensitive info 
+UPDATE `wp_users`  SET 
+`user_email` = CONCAT(user_nicename, '@fake.com');
